e056c49987d7afd63e7389caa74625d8d3f75476
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2023-10-03 17:04:04 +0200

    Create 01.beggining.R

---
+++ 01.beggining.R
@@ -0,0 +1,16 @@
+# Here I can write anything i want! Everyone can see the code and we need to write comments.
+
+# R as a calculator 
+2+3
+
+# Assign to an object 
+zima <- 2+3
+zima
+
+duccio <- 5+3 
+duccio 
+
+final <- zima*duccio 
+final
+
+final^2
