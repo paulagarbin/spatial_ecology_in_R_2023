1d5a3cc28b40a92d3341b2ac0de34c831af5e9f9
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2023-10-10 16:58:39 +0200

    Update 02.populations.2.R

--- 02.populations.2.R
+++ 02.populations.2.R
@@ -7,14 +7,15 @@ install.packages ("spatstat")
 library(spatstat)
 
 #lets use the bei data:
-#data description: link
-bei
-plot(bei)
+#data description: link - that is how we link our data to the code
+bei            # huge amount of data 
+plot(bei)      # huge data points, we need to sort that 
 
 #points are too big so we need to change dimensions 
 #changing dimension - cex
 plot(bei, cex=0.5)   #decreasing by 0.5
-plot(bei, cex=0.2)
+plot(bei, cex=0.2)   # all points are trees in the landscape 
+                     # from this we can create density map
 
 #changing the symbol - pch (19 is filled points)
 plot (bei, cex=0.2, pch=19)
@@ -38,3 +39,27 @@ bei.extra   #elevation was the first element among the two
 #second method to select elements 
 elevation2 <- bei.extra [[1]] #selecting first element and assign it to a variable
 plot(elevation2)
+
+
+#### density map 
+# passing from points to a continuous surface 
+#density is a function in spatstat dataset
+densitymap <- density(bei)     # from points before (bei) now we are dealing with pixels  
+
+plot (densitymap)   # we get density of trees over the area in coloured image 
+
+# let's put points on top of the image 
+# points function is adding on top of something (image)
+
+points(bei, cex=.2)
+
+# let's change colour since it is not good to have red blue and green in the same map for daltonic people 
+colorRampPalette (c("black", "red", "orange", "yellow")(100)    # R and P are capital sensitive 
+                  
+# colours stored in " " 
+# c is a function to merge colours together and use them all
+# 100 is number of colours passing from black to red to orange etc...
+
+cl <- colorRampPalette (c("black", "red", "orange", "yellow"))(100) 
+plot(densitymap, col=cl)
+                   
