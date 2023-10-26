ff4baeb2d925113bdd6acd92ab98d72fc026c759
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2023-10-26 17:02:59 +0200

    Update remote.sensing.visualisation.R

--- remote.sensing.visualisation.R
+++ remote.sensing.visualisation.R
@@ -6,12 +6,17 @@ install.packages("devtools")
 library(devtools) # packages in R are also called libraries
 
 # install the imageRy package from GitHub
-devtools::install_github("ducciorocchini/imageRy")
+devtools::install_github("ducciorocchini/imageRy")      # just like we do install.packages in cran for github it is install_github 
+                                                        # in cran it is controlled, in github anyone can use it at any time
 library(imageRy)
 
 install.packages("terra")
 library(terra)
 
-im.list()
+#list the data
+im.list()         #list of all files that are within imagery 
 
-b2 <- im.import("sentinel.dolomites.b2.tif")   # importing band 2 of Sentinel 2 
+# we will use sentinel dolomites band 2
+im.import("sentinel.dolomites.b2.tif")       # importing band 2 of Sentinel 2 
+b2 <- im.import("sentinel.dolomites.b2.tif") # assign it to an object
+
