202c0ad663a32c092562313a31c12b08ca986169
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2023-10-26 17:42:24 +0200

    Update remote.sensing.visualisation.R

--- remote.sensing.visualisation.R
+++ remote.sensing.visualisation.R
@@ -20,3 +20,10 @@ im.list()         #list of all files that are within imagery
 im.import("sentinel.dolomites.b2.tif")       # importing band 2 of Sentinel 2 
 b2 <- im.import("sentinel.dolomites.b2.tif") # assign it to an object
 
+clb <- colorRampPalette(c("dark grey", "grey", "light grey")) (100)     # colorpalete with 3 colours light grey being the highest - clb is the name 
+plot(b2, col=clb)      
+
+# import the green band from Sentinel-2 (band 3) 
+b3 <- im.import("sentinel.dolomites.b3.tif" )                           # dark is absorbing green colour - light grey reflecting the green
+clb <- colorRampPalette(c("dark grey", "grey", "light grey")) (100)     # colorpalete with 3 colours light grey being the highest - clb is the name 
+plot(b3, col=clb)      
