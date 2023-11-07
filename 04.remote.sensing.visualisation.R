0c356f246e5cf511210eb68a8b0fa3f0ae4c95b8
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2023-11-07 17:32:11 +0100

    Update 04.remote.sensing.visualisation.R

--- 04.remote.sensing.visualisation.R
+++ 04.remote.sensing.visualisation.R
@@ -90,7 +90,40 @@ pairs(stacksent)          # wait for it - plotting many data
                           # green graphs are visible bands (green, blue, infra-red, near infra red)
                           # graphs bringing more or less same information 
                           # all bands correlated (positively) to each other - all between 0.7 and 1.00 
-                 
+                          # values of reflectance in integer - black scatter plots 
+                          # first column blue band 
+                          # histogram - representing frequency of every value of reflectance - in blue band several pixels with
+                          # smaller reflectance and small amount with higher reflectance 
+
+
+##### DIFFERENCE VEGETATION INDEX (DVI)
+# INDEX is a single layer 
+# difference since its a difference between layers
+# you can look at change over time - repeat process after some time and see the change 
+
+# healthy tree (plant)
+# healthy leaves - near infra red - highly reflectance
+# healthy leaves - absorb infra red - low reflectance 
+# lets compare them 
+# high value in near infra red - up to 100 (1-100 is reflectance)
+# low value in infra red since it is absorbed - around 20
+# take near infra red value (high one) minus the infra red information 
+# 100-20 = 80 - difference 
+
+# tree in suffering
+# in another place tree is suffering - drought 
+# reflectance in near infra red is lower - 60 
+# in this case tree is suffering so its capability of photosynthesis is lower - higher reflectance in red (lower absorption) 
+# infra red - 50 
+# 60 - 50 = 10 is the difference 
+
+# we can look at the same bands over the same period 
+# how earthy is the vegetaiton - look at the same difference at same place 
+# if DVI is going from 80 to 10 - forest is getting destroyed - important to ACT or STOP the activity 
+
+
+
+
                           
 
 
