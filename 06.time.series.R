5502517836ed9e8c718837b2c8468e6730326c8f
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2023-11-14 17:21:18 +0100

    Update 06.time.series.R

--- 06.time.series.R
+++ 06.time.series.R
@@ -34,13 +34,34 @@ plot(dif, col=cldif)          # blue continued working - high values
 ##### New example: Greenland increase of temperature
 # Land surface temperature - LST
 
+g2000 <- im.import("greenland.2000.tif")      # at 16 bits - not 8 like usual
+                                              # one layer -just to see how it appears in 2000
+clg <- colorRampPalette(c("blue","white", "red")) (100)
+plot(g2000, col=clg)                          # situation in 2000 - very wide inner area with almost all covered with ice and snow - temperature of the surface
+                                              # middle of Greenland - large area with very low temperature 
+                                              # outside of these area higher temperatures (in red)
+
+# import all the images of Greenland together
+
+g2005 <- im.import("greenland.2005.tif") 
+g2010 <- im.import("greenland.2010.tif") 
+g2015 <- im.import("greenland.2015.tif") 
+
+plot(g2015, col=clg)          # deep blue in central part is decreasing - it is still blue so add another colour for the middle of Greenland (lowest values)
+
+# add black in color ramp palette in the middle 
+clg <- colorRampPalette(c("black","blue","white", "red")) (100)
+plot(g2015, col=clg)    
+
+# plot together 2000 and 2015 to see the difference 
+par(mfrow=c(2,1))          
+plot(g2000, col=clg)
+plot(g2015, col=clg)
 
 
-
-
-
-
-
+# stacking the data 
+stackg <- c(g2000, g2005, g2010, g2015)
+plot(stackg, col=clg)
 
 
 
