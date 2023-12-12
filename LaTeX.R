a03ac07ada06c8256cc849194f27baadabc09676
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2023-12-12 17:58:43 +0100

    Create LaTeX.R

---
+++ LaTeX.R
@@ -0,0 +1,11 @@
+# Why LaTeX 
+## Problems with Word or whatever: 
+
++ Formulas 
++ Layout: header 
++ Figures: numbering 
++ Bibilography 
++....
+
+# possible to make presentations and documents in LaTeX
+# all starts in backslash 
