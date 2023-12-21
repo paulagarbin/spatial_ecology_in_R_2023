d4493524787cbac28264db6b488c7eeb53464223
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2023-12-21 16:50:54 +0100

    Update 10.variability.R

--- 10.variability.R
+++ 10.variability.R
@@ -36,13 +36,29 @@ sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
                   # function = standard deviation - sd 
                   # avoid using just sd as name since it means standard deviation in r
 
-viridis <- colorRampPalette(viridis(7))(255)        # 255 amount of different colors used 
+viridis <- colorRampPalette(viridis(7))(255)        # 7 colors and 255 amount of different inner-colors used 
 plot(sd3, col=viridis)                              # we can see what is the probability in space of near infra red - ultra complex geology is green - very dangerous in glaciar environment
                                                     # also nice ridges where vegetation is present - contrasting element with the rest of the landscape 
+                                                    # northwest highest variability 
 
 # stacking nir and sd
 stacknv <- c(nir, sd3)
 plot(stacknv, col=viridis)
+
+### Exercise: Calculate variability in a 7x7 moving window
+sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)        # final number of pixels is now 49 so 1/49 
+plot(sd7, col=viridis)                               # we can now see more 
+
+# Exercise: plot together via par(mfrow()) the 3x3 and the 7x7 standard deviation
+together <- par(mfrow=c(1,2))
+plot(sd3, col=viridis)           # very local calculation if standard deviation is 3x3 - in infrared only suble differences
+plot(sd7, col=viridis)           # if window enlarged including additional pixels (49 now) - bigger moving window, higher variability in this image 
+                                 # same situation as we will include additional people to the analysis 
+
+# original image plus the 7x7 sd
+par(mfrow=c(1,2))                # high geological or species variability 
+im.plotRGB (sent, r=2, g=1 , b=3)
+plot(sd7, col=viridis)
 
 # change the moving window
 sd5 <- focal(nir, matrix(1/25, 5, 5), fun=sd)
