56d58669a3dca644ac9fd38b3d97956a0952de02
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2024-01-09 17:39:55 +0100

    Create 12.RMarkdown.R

---
+++ 12.RMarkdown.R
@@ -0,0 +1,21 @@
+---
+title: "MarkdownReport"
+author: "Paula Garbin"
+date: "2024-01-09"
+output: html_document
+---
+
+# My first markdown document!            
+ In Rmarkdown # is a title 
+
+Here you can add some text if you want! 
+# RMarkDown file!
+In this case you obtain a result:
+```{r, eval=T}             state to the software what language we are using and if we want to run the code or not 
+ 2+3                       language is r, if code should lead to the result or not (eval=T or F)
+```
+
+In this case you do not:
+```{r, eval=F}            
+ 2+3                     
+```
